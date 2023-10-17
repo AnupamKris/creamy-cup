40c30ac9cd6e746edcafc308885e46313ce8c5b2
AnupamKris <anupamkris13262@gmail.com>
2023-10-17 18:26:33 +0530

    Navigate

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import { useState } from "react";
 import NavBar from "./components/NavBar";
 import CoffeeCard from "./components/CoffeeCard";
-
+import { useNavigate } from "react-router-dom";
 const products = {
   special: {
     image: "/special.png",
@@ -21,6 +21,7 @@ const products = {
 };
 
 const App = () => {
+  const navigate = useNavigate();
   return (
     <div className="app">
       <div className="title">
@@ -28,12 +29,14 @@ const App = () => {
         <img src="/coffee.png" alt="" className="left-img" />
         <div className="text">
           <p>Exquisite Journey into Coorg's Arabica Bliss</p>
-          <span><img src="/logo.png" alt="" /></span>
+          <span>
+            <img src="/logo.png" alt="" />
+          </span>
           <p>
             Discover the Finest Coffee Beans from the Heart of Kodagu, Crafted
             with Love for Your Perfect Brew
           </p>
-          <button>Shop Now</button>
+          <button onClick={() => navigate("/shop")}>Shop Now</button>
         </div>
         <div className="image">
           <img src="/homegrouped.png" alt="" />
--- src/components/CoffeeCard.jsx
+++ src/components/CoffeeCard.jsx
@@ -1,12 +1,13 @@
 import React from "react";
-
+import { useNavigate } from "react-router-dom";
 const CoffeeCard = ({ coffee }) => {
+  const navigate = useNavigate();
   return (
     <div className="coffee-card">
       <img src={coffee.image} alt={coffee.name} />
       <h2>{coffee.name}</h2>
       <p>{coffee.description}</p>
-      <button>Explore</button>
+      <button onClick={() => navigate("/shop")}>Explore</button>
     </div>
   );
 };
